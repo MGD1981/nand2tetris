d65a3a71cc1a321bab305dad903390ba946a8728
gremdavel <MGD1981@gmail.com>
2013-06-25 17:22:44 -0400

    Instruction components stored as strings instead of lists.

--- projects/06/hack-assembler.py
+++ projects/06/hack-assembler.py
@@ -19,18 +19,15 @@ def find_eol(line):
     return eol 
 
 def run_symbol_pass(hack_file):
-    #pdb.set_trace()
-
 
     symbol_table = {'SP':0, 'LCL':1, 'ARG':2, 'THIS':3, 'THAT':4,
-                    'R0':0, 'R1':1, 'R2':2, 'R3':3, 'R4':4, 'R5':5, 'R6':6, 'R7':7,
-                    'R8':8, 'R9':9, 'R10':10, 'R11':11, 'R12':12, 'R13':13,
-                    'R14':14, 'R15':15, 'SCREEN':16384, 'KBD':24576}
+                    'R0':0, 'R1':1, 'R2':2, 'R3':3, 'R4':4, 'R5':5, 'R6':6,
+                    'R7':7, 'R8':8, 'R9':9, 'R10':10, 'R11':11, 'R12':12,
+                    'R13':13, 'R14':14, 'R15':15, 'SCREEN':16384, 'KBD':24576}
 
-    # First Run-Through
     line_number = -1
-    s = open(hack_file, 'r') # s is copy file for first pass
-    for line in s:
+    f = open(hack_file, 'r') 
+    for line in f:
         line = cleanup(line)
         if line == '':
             continue
@@ -42,11 +39,11 @@ def run_symbol_pass(hack_file):
             eol = line.index(')')
             address_label = line[loc+1:eol]
             symbol_table[address_label] = line_number + 1
-    s.close()
+    f.close()
 
     ram = 16
-    s = open(hack_file, 'r') # s is copy file for first pass
-    for line in s:
+    f = open(hack_file, 'r')
+    for line in f:
         line = cleanup(line)
         if line == '':
             continue
@@ -54,23 +51,27 @@ def run_symbol_pass(hack_file):
         if line[loc] == '@':
             eol = find_eol(line) 
             address_label = line[loc+1:eol]
-            if address_label not in symbol_table and not address_label.isdigit():
+            if (address_label not in symbol_table and
+                         not address_label.isdigit()):
                 symbol_table[address_label] = ram
                 ram = ram + 1
-
+    f.close()
     return symbol_table
 
 def run_assembler_pass(hack_file, symbol_table):
-    h = open(hack_file[0:len(hack_file)-4] + '.hack2', 'w') # h is new '.hack' file
-    f = open(hack_file, 'r') # f is original file
+    new_file_name = hack_file.replace('asm', 'hack')
+    h = open(new_file_name, 'w') # h is new '.hack' file
+    f = open(hack_file, 'r')
     # Second Run-Through
     for line in f:
         ignore = False
-        i = [1,1,1]
-        a = [0]
-        c = [0,0,0,0,0,0]
-        d = [0,0,0]
-        j = [0,0,0]
+        i = '111'
+        a = '0'
+        c = '000000'
+        d0 = '0' 
+        d1 = '0'
+        d2 = '0'
+        j = '000'
         line = cleanup(line)
         if line == '':
             continue
@@ -105,77 +106,69 @@ def run_assembler_pass(hack_file, symbol_table):
         if ';J' in line:
             loc = line.index(';J') 
             j_code = line[loc+2:loc+4]
-            if j_code == 'GT': j = [0,0,1]
-            elif j_code == 'EQ': j = [0,1,0]
-            elif j_code == 'GE': j = [0,1,1]
-            elif j_code == 'LT': j = [1,0,0]
-            elif j_code == 'NE': j = [1,0,1]
-            elif j_code == 'LE': j = [1,1,0]
-            elif j_code == 'MP': j = [1,1,1]
+            if j_code == 'GT': j = '001'
+            elif j_code == 'EQ': j = '010'
+            elif j_code == 'GE': j = '011'
+            elif j_code == 'LT': j = '100'
+            elif j_code == 'NE': j = '101'
+            elif j_code == 'LE': j = '110'
+            elif j_code == 'MP': j = '111'
             loc = 0
 
         if '=' in line: # Write the instruction to h
             loc = line.index('=')
             if 'A' in line[:loc]:
-                d[0] = 1
+                d0 = '1'
             if 'D' in line[:loc]:
-                d[1] = 1
+                d1 = '1'
             if 'M' in line[:loc]:
-                d[2] = 1
+                d2 = '1'
             loc += 1
         eol = find_eol(line)  
         comp = line[loc:eol]
         if 'M' in comp:
-            a = 1
+            a = '1'
         if comp == '0':
-            c = [1,0,1,0,1,0]
+            c = '101010'
         elif comp == '1':
-            c = [1,1,1,1,1,1]
+            c = '111111'
         elif comp == '-1':
-            c = [1,1,1,0,1,0]
+            c = '111010'
         elif comp == 'D':
-            c = [0,0,1,1,0,0]
+            c = '001100'
         elif comp == 'A' or comp == 'M':
-            c = [1,1,0,0,0,0]
+            c = '110000'
         elif comp == '!D':
-            c = [0,0,1,1,0,1]
+            c = '001101'
         elif comp == '!A' or comp == '!M':
-            c = [1,1,0,0,0,1]
+            c = '110001'
         elif comp == '-D':
-            c = [0,0,1,1,1,1]
+            c = '001111'
         elif comp == '-A' or comp == '-M':
-            c = [1,1,0,0,1,1]
+            c = '110011'
         elif comp == 'D+1':
-            c = [0,1,1,1,1,1]
+            c = '011111'
         elif comp == 'A+1' or comp == 'M+1':
-            c = [1,1,0,1,1,1]
+            c = '110111'
         elif comp == 'D-1':
-            c = [0,0,1,1,1,0]
+            c = '001110'
         elif comp == 'A-1' or comp == 'M-1':
-            c = [1,1,0,0,1,0]
+            c = '110010'
         elif comp == 'D+A' or comp == 'D+M':
-            c = [0,0,0,0,1,0]
+            c = '000010'
         elif comp == 'D-A' or comp == 'D-M':
-            c = [0,1,0,0,1,1]
+            c = '010011'
         elif comp == 'A-D' or comp == 'M-D':
-            c = [0,0,0,1,1,1]
+            c = '000111'
         elif comp == 'D&A' or comp == 'D&M':
-            c = [0,0,0,0,0,0]
+            c = '000000'
         elif comp == 'D|A' or comp == 'D|M':
-            c = [0,1,0,1,0,1]
+            c = '010101'
 
 
-        newline = []
-        newline.append(i)
-        newline.append(a)
-        newline.append(c)
-        newline.append(d)
-        newline.append(j)
-        newline = str(newline)
-        newline = (newline.replace('[','').replace(']','').replace(
-                   ',','').replace(' ',''))
+        newline = i + a + c + d0 + d1 + d2 + j + '\n' 
         if not ignore: 
-            h.write(newline + '\n')
+            h.write(newline)
 
     h.close()
     f.close()
