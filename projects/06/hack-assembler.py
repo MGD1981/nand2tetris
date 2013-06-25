120354d081ea637153a72ad0d0d7dc91a9507208
gremdavel <MGD1981@gmail.com>
2013-06-25 16:47:03 -0400

    Old changes

--- projects/06/hack-assembler.py
+++ projects/06/hack-assembler.py
@@ -1,9 +1,9 @@
-#! /usr/local/bin/python
+#! /usr/bin/python
 from sys import argv
 import pdb
 
-if __name__ == '__main__':
-    script, hack_file = argv
+
+script, hack_file = argv
 
 def cleanup(line):
     loc = 0
@@ -31,152 +31,155 @@ symbol_table = {'SP':0, 'LCL':1, 'ARG':2, 'THIS':3, 'THAT':4,
                 'R14':14, 'R15':15, 'SCREEN':16384, 'KBD':24576}
 used_addresses = []
 
-# First Run-Through
-line_number = -1
-for line in s:
-    line = cleanup(line)
-    if line == '':
-        continue
-    loc = 0
-    if '@' in line or '=' in line or ';' in line:
-        line_number += 1
+    # First Run-Through
+def populate_symbol_table(s):
+    line_number = -1
+    for line in s:
+        line = cleanup(line)
+        if line == '':
+            continue
+        if '@' in line or '=' in line or ';' in line:
+            line_number += 1
 
-    if line[loc] == '(':
-        eol = line.index(')')
-        address_label = line[loc+1:eol]
-        symbol_table[address_label] = line_number + 1
+        if line[0] == '(':
+            eol = line.index(')')
+            address_label = line[1:eol]
+            symbol_table[address_label] = line_number + 1
 
-    if line[loc] == '@':
-        eol = find_eol(line) 
-        address_label = line[loc+1:eol]
-        if address_label not in symbol_table:
+        if line[0] == '@':
+            eol = find_eol(line) 
+            address_label = line[1:eol]
+            if address_label not in symbol_table:
+                try:
+                    used_addresses.append(int(address_label))
+                except ValueError:
+                    symbol_table[address_label] = None # Creates symbol 
+                                                       # w/ placeholder
+    return (symbol_table, used_addresses)
+
+
+def make_hack(f, symbol_table, used_addresses):
+    # Second Run-Through
+    ram = 16
+    for line in f:
+        ignore = False
+        i = [1,1,1]
+        a = [0]
+        c = [0,0,0,0,0,0]
+        d = [0,0,0]
+        j = [0,0,0]
+        line = cleanup(line)
+        if line == '':
+            continue
+        loc = 0
+        if ('@' not in line and '=' not in line and
+            ';' not in line):
+            ignore = True
+        if line[loc] == '@': # Writes the address to h
+            eol = find_eol(line)
             try:
-                used_addresses.append(int(address_label))
+                address = int(line[loc+1:eol])
             except ValueError:
-                symbol_table[address_label] = None # Creates symbol 
-                                                   # w/ placeholder
-ram = 16
-for address_label in symbol_table: # Assigns addresses to symbols
-    if symbol_table[address_label] == None:
-        while ram in used_addresses:
-            ram += 1
-        symbol_table[address_label] = ram
-        used_addresses.append(ram)
-
-
-# Second Run-Through
-for line in f:
-    ignore = False
-    i = [1,1,1]
-    a = [0]
-    c = [0,0,0,0,0,0]
-    d = [0,0,0]
-    j = [0,0,0]
-    line = cleanup(line)
-    if line == '':
-        continue
-    loc = 0
-    if ('@' not in line and '=' not in line and
-        ';' not in line):
-        ignore = True
-    if line[loc] == '@': # Writes the address to h
-        eol = find_eol(line)
-        try:
-            address = int(line[loc+1:eol])
-        except ValueError:
-            address = line[loc+1:eol]
-            if address not in symbol_table:
-                pdb.set_trace()
-                print "Error: symbol not defined!"
+                address = line[loc+1:eol]
+                if address not in symbol_table:
+                    pdb.set_trace()
+                    print "Error: symbol not defined!"
+                    f.close()
+                    break
+                while ram in used_addresses:
+                    ram += 1
+                address = ram
+                symbol_table[address] = address
+                used_addresses.append(address)
+            if address >= 32768:
+                print "Error: address larger than 15 bits!"
                 f.close()
                 break
-            address = symbol_table[address]            
-        if address >= 32768:
-            print "Error: address larger than 15 bits!"
-            f.close()
-            break
-        address = bin(address)
-        address = address[2:]
-        extra_0s = 16 - len(address)
-        address = '0' * extra_0s + address
-        h.write(address + '\n')
-        ignore = True
-        loc = len(line)-1
+            address = bin(address)
+            address = address[2:]
+            extra_0s = 16 - len(address)
+            address = '0' * extra_0s + address
+            h.write(address + '\n')
+            ignore = True
+            loc = len(line)-1
 
-    if ';J' in line:
-        loc = line.index(';J') 
-        j_code = line[loc+2:loc+4]
-        if j_code == 'GT': j = [0,0,1]
-        elif j_code == 'EQ': j = [0,1,0]
-        elif j_code == 'GE': j = [0,1,1]
-        elif j_code == 'LT': j = [1,0,0]
-        elif j_code == 'NE': j = [1,0,1]
-        elif j_code == 'LE': j = [1,1,0]
-        elif j_code == 'MP': j = [1,1,1]
-        loc = 0
-    
-    if '=' in line: # Write the instruction to h
-        loc = line.index('=')
-        if 'A' in line[:loc]:
-            d[0] = 1
-        if 'D' in line[:loc]:
-            d[1] = 1
-        if 'M' in line[:loc]:
-            d[2] = 1
-        loc += 1
-    eol = find_eol(line)  
-    comp = line[loc:eol]
-    if 'M' in comp:
-        a = 1
-    if comp == '0':
-        c = [1,0,1,0,1,0]
-    elif comp == '1':
-        c = [1,1,1,1,1,1]
-    elif comp == '-1':
-        c = [1,1,1,0,1,0]
-    elif comp == 'D':
-        c = [0,0,1,1,0,0]
-    elif comp == 'A' or comp == 'M':
-        c = [1,1,0,0,0,0]
-    elif comp == '!D':
-        c = [0,0,1,1,0,1]
-    elif comp == '!A' or comp == '!M':
-        c = [1,1,0,0,0,1]
-    elif comp == '-D':
-        c = [0,0,1,1,1,1]
-    elif comp == '-A' or comp == '-M':
-        c = [1,1,0,0,1,1]
-    elif comp == 'D+1':
-        c = [0,1,1,1,1,1]
-    elif comp == 'A+1' or comp == 'M+1':
-        c = [1,1,0,1,1,1]
-    elif comp == 'D-1':
-        c = [0,0,1,1,1,0]
-    elif comp == 'A-1' or comp == 'M-1':
-        c = [1,1,0,0,1,0]
-    elif comp == 'D+A' or comp == 'D+M':
-        c = [0,0,0,0,1,0]
-    elif comp == 'D-A' or comp == 'D-M':
-        c = [0,1,0,0,1,1]
-    elif comp == 'A-D' or comp == 'M-D':
-        c = [0,0,0,1,1,1]
-    elif comp == 'D&A' or comp == 'D&M':
-        c = [0,0,0,0,0,0]
-    elif comp == 'D|A' or comp == 'D|M':
-        c = [0,1,0,1,0,1]
+        if ';J' in line:
+            loc = line.index(';J') 
+            j_code = line[loc+2:loc+4]
+            if j_code == 'GT': j = [0,0,1]
+            elif j_code == 'EQ': j = [0,1,0]
+            elif j_code == 'GE': j = [0,1,1]
+            elif j_code == 'LT': j = [1,0,0]
+            elif j_code == 'NE': j = [1,0,1]
+            elif j_code == 'LE': j = [1,1,0]
+            elif j_code == 'MP': j = [1,1,1]
+            loc = 0
+        
+        if '=' in line: # Write the instruction to h
+            loc = line.index('=')
+            if 'A' in line[:loc]:
+                d[0] = 1
+            if 'D' in line[:loc]:
+                d[1] = 1
+            if 'M' in line[:loc]:
+                d[2] = 1
+            loc += 1
+        eol = find_eol(line)  
+        comp = line[loc:eol]
+        if 'M' in comp:
+            a = 1
+        if comp == '0':
+            c = [1,0,1,0,1,0]
+        elif comp == '1':
+            c = [1,1,1,1,1,1]
+        elif comp == '-1':
+            c = [1,1,1,0,1,0]
+        elif comp == 'D':
+            c = [0,0,1,1,0,0]
+        elif comp == 'A' or comp == 'M':
+            c = [1,1,0,0,0,0]
+        elif comp == '!D':
+            c = [0,0,1,1,0,1]
+        elif comp == '!A' or comp == '!M':
+            c = [1,1,0,0,0,1]
+        elif comp == '-D':
+            c = [0,0,1,1,1,1]
+        elif comp == '-A' or comp == '-M':
+            c = [1,1,0,0,1,1]
+        elif comp == 'D+1':
+            c = [0,1,1,1,1,1]
+        elif comp == 'A+1' or comp == 'M+1':
+            c = [1,1,0,1,1,1]
+        elif comp == 'D-1':
+            c = [0,0,1,1,1,0]
+        elif comp == 'A-1' or comp == 'M-1':
+            c = [1,1,0,0,1,0]
+        elif comp == 'D+A' or comp == 'D+M':
+            c = [0,0,0,0,1,0]
+        elif comp == 'D-A' or comp == 'D-M':
+            c = [0,1,0,0,1,1]
+        elif comp == 'A-D' or comp == 'M-D':
+            c = [0,0,0,1,1,1]
+        elif comp == 'D&A' or comp == 'D&M':
+            c = [0,0,0,0,0,0]
+        elif comp == 'D|A' or comp == 'D|M':
+            c = [0,1,0,1,0,1]
 
 
-    newline = []
-    newline.append(i)
-    newline.append(a)
-    newline.append(c)
-    newline.append(d)
-    newline.append(j)
-    newline = str(newline)
-    newline = (newline.replace('[','').replace(']','').replace(
-               ',','').replace(' ',''))
-    if not ignore: 
-        h.write(newline + '\n')
+        newline = []
+        newline.append(i)
+        newline.append(a)
+        newline.append(c)
+        newline.append(d)
+        newline.append(j)
+        newline = str(newline)
+        newline = (newline.replace('[','').replace(']','').replace(
+                   ',','').replace(' ',''))
+        if not ignore: 
+            h.write(newline + '\n')
+    h.close()
 
-h.close()
+symbol_table, used_addresses = populate_symbol_table(s)
+make_hack(f, symbol_table, used_addresses)
+
 f.close()
