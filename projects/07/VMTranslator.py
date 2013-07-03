c551114c62a76f2a06b1214cfc9287fa2257e0a9
gremdavel <MGD1981@gmail.com>
2013-07-02 20:25:16 -0400

    Working translator for stack arithmetic.  Refactored with HackNumber class.

--- projects/07/VMTranslator.py
+++ projects/07/VMTranslator.py
@@ -1,6 +1,96 @@
 from sys import argv
  
 
+class HackNumber(object):
+    """HackNumbers have unsigned 16-bit binary attributes and bitwise functions.
+    """
+
+    def __init__(self, dec_value):
+        self.dec_value = dec_value
+        self._out_of_bounds_test(dec_value)
+        self.bin_array = self.dec_to_bin_array(dec_value)
+        self.bin_string = ''.join(self.bin_array)
+        if dec_value < 0:
+            self.unsigned_value = self.find_complement(dec_value)
+        else:
+            self.unsigned_value = self.dec_value
+
+    def _out_of_bounds_test(self, dec_value):
+        if dec_value > 32767 or dec_value < -32768:
+            raise TypeError("Value out of bounds for HackNumber object.")
+        else:
+            return
+
+    def dec_to_bin_array(self, dec_number):
+        if dec_number < 0:
+            _number_to_convert = self.find_complement(dec_number)
+        else:
+            _number_to_convert = dec_number
+        _bin_number_unformatted = bin(_number_to_convert)
+        _bin_number_string = _bin_number_unformatted[2:]
+        return self._bit_string_to_array(_bin_number_string)
+
+    def find_complement(self, dec_number):
+        if dec_number < 0:
+            return 65536 + dec_number
+        elif dec_number > 32767:
+            return dec_number - 65536
+        else:
+            raise TypeError("Function only accepts negative numbers", 
+                            "in base 10.")
+
+    def _append_0s(self, bit_array):
+        _extra_0s = 16 - len(bit_array)
+        _extra_0_array = []
+        for i in range(0, _extra_0s):
+            _extra_0_array.append('0')
+        return _extra_0_array + bit_array
+
+    def _bit_string_to_dec(self, bit_string):
+        bin_to_convert = eval("0b"+bit_string)
+        new_dec = int(bin_to_convert)
+        if bit_string[0] == '1':
+            new_dec = self.find_complement(new_dec)
+        return new_dec
+        
+
+    def _bit_string_to_array(self, _bin_number_string):
+        bit_array = []
+        for digit in _bin_number_string:
+            bit_array.append(digit)
+        bit_array = self._append_0s(bit_array)
+        return bit_array
+            
+    def bitand(self, hack_number2):
+        result_array = []
+        for i in range(0,16):
+            result_array.append(str(int(self.bin_array[i]) &
+                          int(hack_number2.bin_array[i])))
+        result_string = ''.join(result_array)
+        result = self._bit_string_to_dec(result_string) 
+        return result
+
+    def bitor(self, hack_number2):
+        result_array = []
+        for i in range(0,16):
+            result_array.append(str(int(self.bin_array[i]) | 
+                          int(hack_number2.bin_array[i])))
+        result_string = ''.join(result_array)
+        result = self._bit_string_to_dec(result_string) 
+        return result
+
+    def bitnot(self):
+        result_array = []
+        for i in range(0,16):
+            if int(self.bin_array[i]) == 1:
+                result_array.append('0')
+            elif int(self.bin_array[i]) == 0:
+                result_array.append('1')
+        result_string = ''.join(result_array)
+        result = self._bit_string_to_dec(result_string) 
+        return result
+        
+
 def cleanup(line):
     line = line.replace(' ','') # Deletes white space
     if '//' in line: # Deletes comments
@@ -17,75 +107,47 @@ def write_to_asm(lines_to_write, asm):
         asm.write(line_to_write + '\n')
     return
 
-def negate(number):
-    neg_number = -1 * number
-    bin_neg_number_uf = bin(neg_number)
-    if bin_neg_number_uf < 0:
-        cutoff = 3
-    else:
-        cutoff = 2
-    bin_neg_number = bin_neg_number_uf[cutoff:]
-    negated_array = binary_negate(bin_neg_number)
-    return negated_array
+def get_line_type(line):
+    return 'operation' # TODO: create entire function returning type
 
-def bin16_to_dec(bin_number):
-    formatted_bin = "0b" + str(bin_number)
-    dec_number = eval(formatted_bin)
-    if dec_number > 32767:
-        dec_number = dec_number-65536
-    return dec_number
+def bitwise_operate(operation):
+    if operation == 'an':
+        value = stack[len(stack)-1].bitand(stack[len(stack)-2])
+    elif operation == 'or':
+        value = stack[len(stack)-1].bitor(stack[len(stack)-2])
+    elif operation == 'no':
+        value = stack[len(stack)-1].bitnot()
+    return value 
 
+def compare_operate(operation):
+    if operation == 'eq':
+        value = -1 * (stack[len(stack)-2].dec_value == 
+                      stack[len(stack)-1].dec_value)
+    elif operation == 'gt':
+        value = -1 * (stack[len(stack)-2].dec_value > 
+                      stack[len(stack)-1].dec_value)
+    elif operation == 'lt':    
+        value = -1 * (stack[len(stack)-2].dec_value < 
+                      stack[len(stack)-1].dec_value)
+    return value
 
-def binary_negate(bin_number):
-    negated_array = [] 
-    bin_array = bitarray(bin_number)
-    sig1_index = (''.join(bin_array)).rfind('1')
-    for digit in bin_array[:sig1_index]:
-        if digit == '0':
-            negated_array.append('1')
-        elif digit == '1':
-            negated_array.append('0')
-    for digit in bin_array[sig1_index:]:
-        negated_array.append(digit)
-    return negated_array
+def arithmetic_operate(operation):
+    if operation == 'ad':
+        value = stack[len(stack)-2].dec_value + stack[len(stack)-1].dec_value
+    if operation == 'su':
+        value = stack[len(stack)-2].dec_value - stack[len(stack)-1].dec_value
+    if operation == 'ne':
+        value = -1 * stack[len(stack)-1].dec_value
+    return value
 
-
-def bitarray(number_to_convert):
-    bit_array = []
-    number_string = str(number_to_convert)
-    for digit in number_string:
-        bit_array.append(digit)
-    return bit_array
-        
-
-def convert_16bit_bin(number):
-    if number < 0: # Returns 16-bit 2's complement
-        complemented_number = negate(number)
-        return complemented_number[len(complemented_number)-16:]
-    else:
-        bin_number_uf = bin(number)
-        bin_number = bin_number_uf[2:]
-        bin_array = bitarray(bin_number)
-        return bin_array
-
-def invert_16bit_bin(number_to_invert):
-    inverted_array = []
-    bin_array = convert_16bit_bin(number_to_invert)
-    if len(bin_array) < 16:
-        extra_0s = 16 - len(bin_array)
-    for each in range(0, extra_0s):
-        inverted_array.append('1')
-    for digit in bin_array:
-        if digit == '0':
-            inverted_array.append('1')
-        elif digit == '1':
-            inverted_array.append('0')
-    number_string = ''.join(inverted_array)
-    if eval(number_string) < 0:
-        number_string = ''.join(negate(eval(number_string)))
-    dec_number = bin16_to_dec(eval(number_string))
-    return dec_number
-    
+def operate(operation):
+    if operation in ('an', 'or', 'no'):
+        value = bitwise_operate(operation)
+    elif operation in ('eq', 'gt', 'lt'):
+        value = compare_operate(operation)
+    elif operation in ('ad', 'su', 'ne'):
+        value = arithmetic_operate(operation)
+    return value        
 
 def pass_through(vm_file):
     global stack
@@ -97,32 +159,6 @@ def pass_through(vm_file):
                        'lt':'<', 'an':'&', 'or':'|'}
     un_command_list = {'ne':'-', 'no':'!'} 
 
-    def get_line_type(line):
-        return 'arithmetic' # TODO: create entire function returning type
-
-        
-
-    def bi_operate(operation):
-        if operation in ('eq'): 
-            operand = 2 * bi_command_list[operation]
-        else:
-            operand = bi_command_list[operation]
-        if operation in ('gt', 'lt', 'eq'): # Comparative operations
-            value = -1 * eval("stack[len(stack) - 2] %s stack[len(stack) - 1]" 
-                              % operand) # Python True = 1, VM True = -1.
-        else: # Calculative operations
-            value = eval("stack[len(stack) - 1] %s stack[len(stack) - 2]"
-                         % operand) 
-        return value
-
-    def un_operate(operation):
-        operand = un_command_list[operation]
-        if operand == '!':
-            return invert_16bit_bin(stack[len(stack) - 1]) 
-        elif operand == '-':
-            bin_array = convert_16bit_bin(-1 * (stack[len(stack) - 1]))
-            return eval(''.join(bin_array))
-
     for line in f:
     
         lines_to_write = [] # Initizalize line to write to new .asm file.
@@ -133,11 +169,12 @@ def pass_through(vm_file):
 
         line_type = get_line_type(line)
 
-        if line_type == 'arithmetic':
+        if line_type == 'operation':
+
             
             if line[0:4] == 'push':
-                stack.append(int(line[12:eol+1]))
-                lines_to_write = ['@%d' % stack[len(stack) - 1],
+                stack.append(HackNumber(int(line[12:eol+1])))
+                lines_to_write = ['@%d' % stack[len(stack) - 1].dec_value,
                                   'D=A',
                                   '@SP',
                                   'A=M',
@@ -151,21 +188,27 @@ def pass_through(vm_file):
                 if operation in bi_command_list:
                     lines_to_write = ['@SP',
                                       'M=M-1',
+                                      'M=M-1',
                                       'A=M',
                                       'D=M',
                                       '@SP',
-                                      'M=M-1',
+                                      'M=M+1',
                                       'A=M']
-                    value = bi_operate(operation)
+                    value = operate(operation)
                     if operation in ('eq', 'gt', 'lt'):
-                        lines_to_write.append('M=%d' % value)
+                        lines_to_write.append('D=%d' % value)
                     else:
-                        lines_to_write.append('M=D%sM'
+                        lines_to_write.append('D=D%sM'
                                               % bi_command_list[operation])
-                    lines_to_write.extend(('@SP', 'M=M+1'))
+                    lines_to_write.extend(('@SP',
+                                           'M=M-1',
+                                           'A=M',
+                                           'M=D',
+                                           '@SP',
+                                           'M=M+1'))
                     stack.pop()
                     stack.pop()
-                    stack.append(value)
+                    stack.append(HackNumber(value))
 
                 if operation in un_command_list:
                     lines_to_write = ['@SP',
@@ -174,14 +217,14 @@ def pass_through(vm_file):
                                       'M=%sM' % un_command_list[operation],
                                       '@SP',
                                       'M=M+1']
-                    value = un_operate(operation)
-                    print value
+                    value = operate(operation)
                     stack.pop()
-                    stack.append(value)
+                    stack.append(HackNumber(value))
         
         write_to_asm(lines_to_write, asm)
     f.close()
     asm.close()
+
 
 if __name__ == '__main__':
     script, vm_file = argv
